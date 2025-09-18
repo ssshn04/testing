07fe4622c0e976d57f1d840bf79ce7e52ffaca96
ssshn04 <ssshn04@gmail.com>
2025-09-18 23:00:55 +0300

    Added README and refactored code

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,50 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        humanIMB humanIMB = new humanIMB(80,1.52);
+        System.out.println(humanIMB.Result());
+    }
+}
+class humanIMB {
+    public double W; //Weight Human
+    public double H; // Height Human
+    private static double imb;
+    public humanIMB(double w, double h) {
+        W = w;
+        H = h;
+        imb = W / (H * H);
+    }
+    public double takeW() {
+        return W;
+    }
+    public void putW(double w) {
+        W = w;
+        imb = W / (H * H);
+    }
+    public double takeH() {
+        return H;
+    }
+    public void putH(double h) {
+        H = h;
+        imb = W / (H * H);
+    }
+    public static double takeImt() {
+        return imb;
+    }
+    public static String Result() {
+        String  string = null;
+        if (imb >=18.5 & imb <25) {
+            string ="Norm";
         }
+        if (imb >=25 & imb <30) {
+            string ="Warning! ";
+        }
+        if (imb >=30) {
+            string ="Fat";
+        }
+        if (imb <18.5) {
+            string ="Deficit";
+        }
+        return string;
     }
 }
